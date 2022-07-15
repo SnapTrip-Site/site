96153ecc7641a86f76fedf3ea57feb8795b0a1d1
vitor Tocolli <vitorbarbosatocolli@gmail.com>
2022-07-15 14:58:49 -0300

    sextou

--- meu-projeto/app.js
+++ meu-projeto/app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var resultadosRouter = require('./routes/resultados');
 var app = express();
 
 // view engine setup
@@ -20,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/resultados', resultadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,5 +46,7 @@ app.set('view engine', 'ejs');
 
 // adicionar o local de arquivos estaticos 
 app.use(express.static('public'));
+
+
 
 
