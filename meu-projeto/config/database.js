b93460dafc894a1202679e2f561b8b404f185e12
vitor Tocolli <vitorbarbosatocolli@gmail.com>
2022-08-16 14:43:30 -0300

    instalando sequelize

---
+++ meu-projeto/config/database.js
@@ -0,0 +1,8 @@
+const config = {
+    "username":"root",
+    "password":"",
+    "database":"",
+    "host":"",
+    "dialect":"mysql",
+    "operatorsAliases": false //true ou false meti um false só pra n ficar aparecendo erro :)
+}
