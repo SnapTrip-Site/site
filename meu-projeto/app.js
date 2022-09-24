6cec8cad3e992494b440804205048eb753b98152
Antonio <marbergertony@gmail.com>
2022-09-24 17:30:57 -0300

    Adicionando Formulario

--- meu-projeto/app.js
+++ meu-projeto/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./src/routes/index');
 var resultadosRouter = require('./routes/resultados');
 var estadiaRotas = require('./routes/estadiaRotas');
 var passagemRotas = require ('./routes/passagemRotas');
