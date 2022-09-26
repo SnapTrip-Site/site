86f9d64ff8b28313960b997a157a464ace5f8ecd
Gustavo Welter <gustavo-welter@hotmail.com>
2022-09-26 17:15:16 -0300

    Salvando login e senha no BD e autenticando Usuario para Login

--- meu-projeto/app.js
+++ meu-projeto/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var resultadosRouter = require('./routes/resultados');
 var estadiaRotas = require('./routes/estadiaRotas');
 // var passagemRotas = require ('./routes/passagemRotas');
-// var usersRouter = require('./routes/usersRouter');
+var usersRouter = require('./routes/usersRouter');
 var cidadesRoutes = require('./routes/cidadesRoutes');
 var app = express();
 
@@ -26,8 +26,8 @@ app.use('/', indexRouter);
 app.use('/resultados', resultadosRouter);
 app.use('/estadias', estadiaRotas);
 // app.use('/passagens', passagemRotas);
-// app.use('/users', usersRouter);
-app.use('/cidades', cidadesRoutes);
+app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
